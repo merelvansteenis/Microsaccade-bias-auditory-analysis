b8e6e66a13efeb08cd22348182f2ce01e87297a4
Merelvansteenis <merelvansteenis2003@gmail.com>
2026-05-07 10:25:26 +0200

    changing pathway to folder

--- getSubjParam.m
+++ getSubjParam.m
@@ -5,7 +5,7 @@ function param = getSubjParam(pp)
 %% set path and pp-specific file locations
 unique_numbers = [48, 39, 43, 32, 70, 30, 21, 79, 69, 94, 42, 53]; %needs to be in the right order
 
-param.path = '/Users/merelvansteenis/Documents/m6 - auditory';
+param.path = '/Users/merelvansteenis/Documents/Cognitive Neuropsychology/PSR/m6 - auditory';
 
 if pp < 10
     param.subjName = sprintf('pp0%d', pp);
